978f6c1e6432cc10fe860c8eed1b334e59faec73
malte <malteharms@outlook.de>
2022-12-29 14:11:57 +0100

    Initial commit - FX

---
+++ src/main/java/de/malte/f1strat/HelloController.java
@@ -0,0 +1,14 @@
+package de.malte.f1strat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module de.malte.f1strat {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens de.malte.f1strat to javafx.fxml;
+    exports de.malte.f1strat;
+}
