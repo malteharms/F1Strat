56bc086ebe9cdf4fb1763c3134e188f672feb984
malte <malteharms@outlook.de>
2023-01-06 00:58:53 +0100

    First GUI example, now the test data should be removed with real data from json file

--- src/main/java/de/malte/f1strat/StartApplication.java
+++ src/main/java/de/malte/f1strat/StartApplication.java
@@ -1,67 +1,30 @@
 package de.malte.f1strat;
 
-import de.malte.f1strat.handler.JsonHandler;
-import de.malte.f1strat.logic.TyreStrategy;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.Arrays;
 
 public class StartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(StartApplication.class.getResource("start-view.fxml"));
-        stage.setTitle("Hello!");
+        stage.setTitle("F1 | Strategy");
 
-        final NumberAxis xAxis = new NumberAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        xAxis.setLabel("Number of Month");
-        //creating the chart
-        final LineChart<Number,Number> lineChart =
-                new LineChart<Number,Number>(xAxis,yAxis);
+        Parent root = fxmlLoader.load();
+        StartController controller = fxmlLoader.getController();
+        Scene scene = new Scene(root);
 
-        lineChart.setTitle("Rundenzeiten");
-        //defining a series
-        XYChart.Series series = new XYChart.Series();
-        series.setName("Soft");
-        //populating the series with data
-        series.getData().add(new XYChart.Data<>(1, 23));
-        series.getData().add(new XYChart.Data<>(2, 14));
-        series.getData().add(new XYChart.Data<>(3, 15));
-        series.getData().add(new XYChart.Data<>(4, 24));
-        series.getData().add(new XYChart.Data<>(5, 34));
-        series.getData().add(new XYChart.Data<>(6, 36));
-        series.getData().add(new XYChart.Data<>(7, 22));
-        series.getData().add(new XYChart.Data<>(8, 45));
-        series.getData().add(new XYChart.Data<>(9, 43));
-        series.getData().add(new XYChart.Data<>(10, 17));
-        series.getData().add(new XYChart.Data<>(11, 29));
-        series.getData().add(new XYChart.Data<>(12, 25));
-
-        Scene scene = new Scene(lineChart,800,600);
-        lineChart.getData().add(series);
+        controller.fillWithData();
 
         stage.setScene(scene);
-
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
-
-        final String location = "f1-data.json";
-
-        // read file and parse it to object
-        JsonHandler jsonHandler = new JsonHandler();
-        DataInstance data = jsonHandler.loadData(location);
-
-        TyreStrategy bahrainStrategy = new TyreStrategy(data.dry.tracks.bahrain.trackData.fifty);
-
+        launch(args);
     }
 }
--- src/main/java/de/malte/f1strat/StartController.java
+++ src/main/java/de/malte/f1strat/StartController.java
@@ -1,14 +1,42 @@
 package de.malte.f1strat;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
 
 public class StartController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button loadData;
+
+    @FXML
+    private LineChart<Number, Number> lcLapTimes;
+
+    @FXML
+    public void initialize() {
+        loadData.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // loadData button is clicked
+
+
+            }
+        });
+    }
+
+    public void fillWithData() {
+        // line chart
+        lcLapTimes.setTitle("Rundenzeiten pro Reifen");
+
+        XYChart.Series<Number, Number> soft = new XYChart.Series<>();
+        soft.setName("Soft");
+        soft.getData().add(new XYChart.Data<>(1.0, 3.0));
+        soft.getData().add(new XYChart.Data<>(2.0, 6.0));
+        soft.getData().add(new XYChart.Data<>(3.0, 7.0));
+
+        lcLapTimes.getData().add(soft);
     }
 }
